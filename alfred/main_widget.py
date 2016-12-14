51e6fc2c723fd3153b2f8d31ddc37a9165075ac4
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2016-12-14 21:09:01 +0200

    #7 - Basic implementation and usage of ABaseModule

--- alfred/main_widget.py
+++ alfred/main_widget.py
@@ -1,5 +1,5 @@
 # Qt imports
-from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot, QObjectCleanupHandler
+from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QDialog
 
 from .ui.widget_ui import Ui_Dialog
@@ -24,19 +24,3 @@ class MainWidget(QDialog, Ui_Dialog):
         if msg != '' and self.last_text != msg:
             self.text_changed.emit(msg)
             self.last_text = msg
-
-    def remove_viewport_layout(self):
-        if self.scrollAreaWidgetContents.layout() == None:
-            return
-
-        layout = self.scrollAreaWidgetContents.layout()
-
-        for i in range(layout.count()):
-            if layout.itemAt(i) != None:
-                QObjectCleanupHandler().add(layout.itemAt(i).widget())
-
-        QObjectCleanupHandler().add(self.scrollAreaWidgetContents.layout())
-
-    def set_viewport_layout(self, layout):
-        self.remove_viewport_layout()
-        self.scrollAreaWidgetContents.setLayout(layout)
