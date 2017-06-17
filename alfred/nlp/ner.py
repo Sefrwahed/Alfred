ade744a22acd78ca1f9e166ab2e825836ab945af
MennaCC <menna.elsisy@gmail.com>
2017-06-17 15:07:36 +0200

    #46 - Added duckling python wrapper dependency and duckling instantiation to karam's latest ner class

---
+++ alfred/nlp/ner.py
@@ -0,0 +1,31 @@
+import spacy
+
+from alfred import alfred_globals as ag
+from alfred.utils import Singleton
+from duckling import DucklingWrapper
+
+class NER(metaclass=Singleton):
+    def __init__(self):
+        self.spacyNlp = spacy.load('en', parser=None)
+        self.ducklingInstance = DucklingWrapper()
+
+
+    def getNERDuckling(self, text):
+        entities = self.ducklingInstance.parse(text)
+        print(entities)
+        return entities
+
+    def getNERSpacy(self, text):
+        doc = self.spacyNlp(text)
+        entities = {}
+        for ent in doc.ents:
+            if ent.label_ in entities:
+                entities[ent.label_].append(ent)
+            else:
+                entities[ent.label_] = [ent]
+        return entities
+
+    def getNameEntities(self, entities_types_list, text):
+        entities = self.getNERSpacy(text)
+        return {entities_type: entities[entities_type]
+                for entities_type in entities_types_list}
