b8b08ed83617ffa5548f1f487107b3dc4ccab516
MennaCC <menna.elsisy@gmail.com>
2017-06-19 02:07:56 +0200

    #46 - now we have a complete parsing functionality that can be used pre and post classification using both Spacy and Duckling"

--- alfred/nlp/ner_parsers/duckling.py
+++ alfred/nlp/ner_parsers/duckling.py
@@ -1,28 +1,24 @@
-from alfred.utils import Singleton
-from .ner import NER
-
 from duckling import DucklingWrapper
+
+from alfred.nlp.ner_parsers.ner import NER
+from alfred.utils import Singleton
 
 
 class Duckling(NER, metaclass=Singleton):
 
-    def __init__(self):
+    def __init__(self, entities_types):
+        NER.__init__(entities_types)
         self.ducklingInstance = DucklingWrapper()
 
-    def getNER(self, entities_type_list, text):
+    def getNER(self, text):
+        pass
+
+    def getNameEntities(self, text):
         entities = []
-        for dimType in entities_type_list:
+        for dimType in self.entities_types_list:
             method_name = "parse_" + dimType
             entity = getattr(self.ducklingInstance, method_name)(text)
             print(entity)
             entities.append(entity)
-
-        return entities
-
-    def getNameEntities(self, entities_types_list, text):
-        entities = self.getNER(entities_types_list, text)
         return {entity[0]["dim"]: entity[0]["value"]["value"]
                 for entity in entities}
-
-
-
--- alfred/nlp/ner_parsers/ner.py
+++ alfred/nlp/ner_parsers/ner.py
@@ -3,18 +3,21 @@ from abc import abstractmethod
 
 
 class NER(metaclass=Singleton):
-    def __init__(self, entities_list, text):
-        pass
+    def __init__(self, entities_types):
+        self.entities_types_list = entities_types
+
+    def resetEntitiesTypes(self, entities_types):
+        self.entities_types_list = entities_types
+
 
     @abstractmethod
-    def getNER(self, entities_types_list, text):
+    def getNER(self, text):
         """
         Get the entities extracted either from spacy or duckling
         """
 
     @abstractmethod
-    def getNameEntities(self, entities_types_list, text):
+    def getNameEntities(self, text):
         """
         parse the entities extracted either from spacy or duckling and return it
         """
-
